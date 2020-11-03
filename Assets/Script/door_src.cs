8c189dab07bed3cc7f298f44c920bd1f10f66310
y-a <ya48827jb4.sonet.ne.jp@gmail.com>
2020-11-03 14:46:00 +0900

    長谷君が前にやった作業

---
+++ Assets/Script/door_src.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class door_src : MonoBehaviour
+{
+    public GameObject door;//Hingjointを付けている子オブジェクトをセットする
+    Rigidbody rb;
+
+    void Start()
+    {
+        rb = door.GetComponent<Rigidbody>();
+    }
+
+    public void Open()
+    {
+        //ドアを開ける
+        rb.isKinematic = false;
+    }
+}
