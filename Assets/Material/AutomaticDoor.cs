33d99ab7953f1ba116c445630838072601cb183f
masiron0620 <70056253+masiron0620@users.noreply.github.com>
2020-11-15 20:45:20 +0900

    敵の索敵範囲可視化、青人の追跡実装

---
+++ Assets/Material/AutomaticDoor.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutomaticDoor : MonoBehaviour
+{
+    //　ドアのアニメーター
+    private Animator animator;
+
+    void Start()
+    {
+        // 親のAnimatorを取得
+        animator = transform.parent.GetComponent<Animator>();
+    }
+
+    /// <summary>
+    /// 自動ドア検知エリアに入った時
+    /// </summary>
+    /// <param name="other"></param>
+	private void OnTriggerEnter(Collider other)
+    {
+        // アニメーションパラメータをtrueにする。(ドアが開く)
+        animator.SetBool("Open", true);
+    }
+
+    /// <summary>
+    /// 自動ドア検知エリアを出た時
+    /// </summary>
+    /// <param name="other"></param>
+	private void OnTriggerExit(Collider other)
+    {
+        // アニメーションパラメータをfalseにする。(ドアが閉まる)
+        animator.SetBool("Open", false);
+    }
+}
---
+++ Assets/Script/MoveEnemy.cs
@@ -0,0 +1,138 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveEnemy : MonoBehaviour
+{
+
+    public enum EnemyState
+    {
+        Walk,
+        Wait,
+        Chase
+    };
+
+    private CharacterController enemyController;
+    private Animator animator;
+    //　目的地
+    private Vector3 destination;
+    //　歩くスピード
+    [SerializeField]
+    private float walkSpeed = 1.0f;
+    //　速度
+    private Vector3 velocity;
+    //　移動方向
+    private Vector3 direction;
+    //　到着フラグ
+    private bool arrived;
+    //　SetPositionスクリプト
+    private SetPosition setPosition;
+    //　待ち時間
+    [SerializeField]
+    private float waitTime = 5f;
+    //　経過時間
+    private float elapsedTime;
+    // 敵の状態
+    private EnemyState state;
+    //　プレイヤーTransform
+    private Transform playerTransform;
+
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine("Stop");
+        enemyController = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
+        setPosition = GetComponent<SetPosition>();
+        setPosition.CreateRandomPosition();
+        velocity = Vector3.zero;
+        arrived = false;
+        elapsedTime = 0f;
+        SetState(EnemyState.Walk);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        IEnumerator Stop()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))//  もし、スペースキーがおされたなら、  
+            {
+                walkSpeed = 0.0f;
+                yield return new WaitForSeconds(5.0f);
+                walkSpeed = 1.0f;
+            }
+        }
+
+        //　見回りまたはキャラクターを追いかける状態
+        if (state == EnemyState.Walk || state == EnemyState.Chase)
+        {
+            //　キャラクターを追いかける状態であればキャラクターの目的地を再設定
+            if (state == EnemyState.Chase)
+            {
+                setPosition.SetDestination(playerTransform.position);
+            }
+            if (enemyController.isGrounded)
+            {
+                velocity = Vector3.zero;
+                animator.SetFloat("Speed", 2.0f);
+                direction = (setPosition.GetDestination() - transform.position).normalized;
+                transform.LookAt(new Vector3(setPosition.GetDestination().x, transform.position.y, setPosition.GetDestination().z));
+                velocity = direction * walkSpeed;
+            }
+
+            //　目的地に到着したかどうかの判定
+            if (Vector3.Distance(transform.position, setPosition.GetDestination()) < 0.5f)
+            {
+                SetState(EnemyState.Wait);
+                animator.SetFloat("Speed", 0.0f);
+            }
+            //　到着していたら一定時間待つ
+        }
+        else if (state == EnemyState.Wait)
+        {
+            elapsedTime += Time.deltaTime;
+
+            //　待ち時間を越えたら次の目的地を設定
+            if (elapsedTime > waitTime)
+            {
+                SetState(EnemyState.Walk);
+            }
+        }
+        velocity.y += Physics.gravity.y * Time.deltaTime;
+        enemyController.Move(velocity * Time.deltaTime);
+    }
+
+    //　敵キャラクターの状態変更メソッド
+    public void SetState(EnemyState tempState, Transform targetObj = null)
+    {
+        if (tempState == EnemyState.Walk)
+        {
+            arrived = false;
+            elapsedTime = 0f;
+            state = tempState;
+            setPosition.CreateRandomPosition();
+        }
+        else if (tempState == EnemyState.Chase)
+        {
+            state = tempState;
+            //　待機状態から追いかける場合もあるのでOff
+            arrived = false;
+            //　追いかける対象をセット
+            playerTransform = targetObj;
+        }
+        else if (tempState == EnemyState.Wait)
+        {
+            elapsedTime = 0f;
+            state = tempState;
+            arrived = true;
+            velocity = Vector3.zero;
+            animator.SetFloat("Speed", 0f);
+        }
+    }
+    //　敵キャラクターの状態取得メソッド
+    public EnemyState GetState()
+    {
+        return state;
+    }
+}
